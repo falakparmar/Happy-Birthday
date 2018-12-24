755ba47f876cd2ed399136ad1c52d019b238f39d
dell-pc <falak.p19@gmail.com>
2018-12-24 21:06:51 +0530

    Hello World ! Good Day ! God Bless Me !!!

---
+++ app/src/main/java/com_app/example/android/happybday/MainActivity.java
@@ -0,0 +1,13 @@
+package com_app.example.android.happybday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
